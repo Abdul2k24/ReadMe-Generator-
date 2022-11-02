840d14ff336368a2e8e8865d9a698abadb3aa28a
Mr. Abdulkadir <mr.abdulkadir@Maymunas-MacBook-Air.local>
2022-11-01 21:56:33 -0500

    completed array questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,11 +5,51 @@ const fs = require('fs')
 
 // TODO: Create an array of questions for user input
 const questions = [{
-    type: "input"
-    message: "What is the name of your project?"
+    type: "input",
+    message: 'What is the name of your project?',
     name: "Title"
-
-}];
+},
+{
+    type: 'input',
+    message: 'What is your Github name?',
+    name: 'Username'
+},
+{
+    type: 'input',
+    message: 'What is your Project about?',
+    name: 'Description'
+},
+{
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'Email'
+},
+{
+    type: 'input',
+    message: 'What does the user need to install?',
+    name: 'Installation'
+},
+{
+    type: 'input',
+    message: 'How does the user use this Repository?',
+    name: 'Usage'
+},
+{
+    type: 'input',
+    message: 'What contributed to this project?',
+    name: 'Contribute'
+},
+{
+    type: 'input',
+    message: 'What is need to run this command test?',
+    name: 'Test'
+},
+{
+    type: 'input',
+    message: 'What type of lisense is being used for this usage?',
+    name: 'License'
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
